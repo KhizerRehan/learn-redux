c0039134b45a1cd8b3533c60653892fe1e0e04e9
khizerrehan <khizerrehan92@gmail.com>
2022-01-30 16:53:22 +0500

    fem-redux-fundamentals-by-steve - fix comment

--- src/basic-store.js
+++ src/basic-store.js
@@ -39,13 +39,13 @@ const reducer = (state = initialState, action) => {
 
 
 /*
-    Note: This code is working due to `setTimeout` which is correct sicne 
+    Note: This code is working due to `setTimeout` which is correct since 
     you have imported this file in index.js top so if you try to to do 
 
     `store.dispatch(incrementActionCreator());` without timeout it will 
     throw an error -> "Cannot access 'store' before initialization" so 
     you here a patch or hacky way is applied to async wait using setTimeout
-    to mimic async behaviour and let JS to execute below code to let state
+    to mimic async behaviour and let JS to execute below code to let `state`
     variable initialized.
 */
 // Call Increment after 1s
